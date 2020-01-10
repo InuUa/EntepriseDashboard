da9a7aa20c3bb0d47f07d9c5900c5699cfc61de6
Ej <Ejgithinji@gmail.com>
2020-01-10 09:15:38 +0300

    Updates

---
+++ precache-manifest.fab5ad565301e7ebb9fa94ad58f0672d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15b11560df294a9a206da96fe009996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e54dfe38c683c92751ea",
+    "url": "/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "f4dbab1eceab3cd0d961",
+    "url": "/static/css/main.d15e702f.chunk.css"
+  },
+  {
+    "revision": "e54dfe38c683c92751ea",
+    "url": "/static/js/2.0ffde1c3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0ffde1c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4dbab1eceab3cd0d961",
+    "url": "/static/js/main.8b67ee85.chunk.js"
+  },
+  {
+    "revision": "3cd4bf6061fb88a0778b",
+    "url": "/static/js/runtime-main.2ee504bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
